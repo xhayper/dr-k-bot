38a8d0b1859497fe2cabd8b5892337bd92c9c848
xhayper <hayper1919@gmail.com>
2022-04-24 01:03:40 +0700

    feat: Moved to winston

---
+++ src/logger.ts
@@ -0,0 +1,25 @@
+import Winston from 'winston';
+import path from 'path';
+
+const logger = Winston.createLogger({
+  level: process.env.DEBUG === 'true' ? 'debug' : 'info',
+  format: Winston.format.combine(
+    Winston.format.errors({ stack: true }),
+    Winston.format.timestamp({
+      format: 'DD-MM-YYYY HH:MM:SS'
+    }),
+    Winston.format.printf((msg) =>
+      Winston.format
+        .colorize()
+        .colorize(msg.level, `[${msg.timestamp}] [${msg.level.toUpperCase()}] ${msg.stack || msg.message}`)
+    )
+  ),
+  transports: [
+    new Winston.transports.Console(),
+    // Normal
+    new Winston.transports.File({ filename: path.join(__dirname, '../logs/latest.log'), options: { flags: 'w' } }),
+    new Winston.transports.File({ filename: path.join(__dirname, `../logs/${new Date().toISOString()}.log`) })
+  ]
+});
+
+export { logger as Logger };
