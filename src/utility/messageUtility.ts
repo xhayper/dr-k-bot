1b4065f09007f06eec98003bf300c795210a8d91
xhayper <hayper1919@gmail.com>
2022-04-24 11:00:29 +0700

    feat: formatting again

--- src/utility/messageUtility.ts
+++ src/utility/messageUtility.ts
@@ -5,7 +5,7 @@ export class MessageUtility {
     return `${message.content}${message.content.trim() != '' && message.attachments.size > 0 ? '\n\n' : ''}${Array.from(
       message.attachments.values()
     )
-      .map((attachment, index) => `[Attachment ${index + 1} | ${attachment.name}](${attachment.proxyURL})`)
+      .map((attachment, index) => `[| Attachment ${index + 1} | ${attachment.name} |](${attachment.proxyURL})`)
       .join('\n')}`;
   }
 
