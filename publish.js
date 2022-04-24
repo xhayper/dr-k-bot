93de05e08ecb799f9a61caaba68da72fdda32294
xhayper <46065349+xhayper@users.noreply.github.com>
2022-04-24 11:19:45 +0700

    remove: Unpublish code

--- publish.js
+++ publish.js
@@ -61,21 +61,6 @@ const command = [
   const restClient = new REST().setToken(process.env.DISCORD_TOKEN);
 
   const user = await restClient.get(Routes.user());
-  const guildList = await restClient.get(Routes.userGuilds());
-
-  for (const guild of guildList) {
-    const commandList = await restClient.get(Routes.applicationGuildCommands(user.id, guild.id));
-
-    for (const command of commandList) {
-      await restClient.delete(Routes.applicationGuildCommand(user.id, guild.id, command.id));
-    }
-  }
-
-  const currentCommandList = await restClient.get(Routes.applicationCommands(user.id));
-
-  for (const command of currentCommandList) {
-    await restClient.delete(Routes.applicationCommand(user.id, command.id));
-  }
 
   restClient.put(Routes.applicationCommands(user.id), {
     body: command.map((builder) => builder.toJSON())
