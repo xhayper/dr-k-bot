3761ce10e0023747c434847376973fd67a08298e
xhayper <hayper1919@gmail.com>
2022-04-25 20:19:06 +0700

    feat: Insult system

--- src/event/messageCreate.ts
+++ src/event/messageCreate.ts
@@ -1,16 +1,29 @@
 import { TypedEvent } from '../base/clientEvent';
 import { Client, Message } from 'discord.js';
-import { GuildUtility } from '..';
+import path from 'path';
+import fs from 'fs';
+
+const insultList = JSON.parse(fs.readFileSync(path.join(__dirname, '../../insult.json'), 'utf8')) as string[];
 
 export default TypedEvent({
   eventName: 'messageCreate',
-  on: async (_: Client, message: Message) => {
-    // if (!message.member || message.author.bot || GuildUtility.isModerator(message.member)) return;
-    // if (
-    //   ![/KP/gim, /Kaiju Paradise/gim, /TFE/gim, /The Finale Experiment/gim].some((regex) => regex.test(message.content))
-    // )
-    //   return;
-    // await message.reply('Please, Don\'t mention "Kaiju Paradise" or "The Finale Experiment"');
-    // await message.delete();
+  on: async (client: Client, message: Message) => {
+    if (message.author.bot || message.channel.type == 'DM') return;
+
+    const splitText = message.content.split(' ');
+
+    if (
+      message.mentions.users.size == 0 ||
+      0 >= splitText.length ||
+      ![`<@${client.user!.id}>`, `<@!${client.user!.id}>`].some((mentionText) => splitText[0].startsWith(mentionText))
+    )
+      return;
+
+    await message.reply({
+      content: insultList[Math.floor(Math.random() * insultList.length)],
+      allowedMentions: {
+        repliedUser: false
+      }
+    });
   }
 });
