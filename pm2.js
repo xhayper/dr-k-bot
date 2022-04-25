d5cf2ed39ff1971a4adcedcf7329a346f1d9c2a4
xhayper <hayper1919@gmail.com>
2022-04-25 09:45:02 +0700

    feat: PM2, Please

--- pm2.js
+++ pm2.js
@@ -1,6 +1,34 @@
 require('dotenv/config');
 
-const  { exec } = require('child_process');
+const { execSync } = require('child_process');
 
-exec(`pm2 link ${process.env.PM2_PUBLIC_KEY} ${process.env.PM2_SECRET_KEY}`);
-exec(`pm2 start ecosystem.config.js`);
+execSync(`pm2 link ${process.env.PM2_PUBLIC_KEY} ${process.env.PM2_SECRET_KEY}`);
+
+pm2.connect(function (err) {
+  if (err) {
+    console.error(err);
+    process.exit(2);
+  }
+
+  pm2.start(
+    {
+      script: 'dist/index.js',
+      name: 'dr-k-bot'
+    },
+    (err, apps) => {
+      if (err) {
+        console.error(err);
+        return pm2.disconnect();
+      }
+
+      pm2.list((err, list) => {
+        console.log(err, list);
+
+        pm2.restart('dr-k-bot', (err, proc) => {
+          // Disconnects from PM2
+          pm2.disconnect();
+        });
+      });
+    }
+  );
+});
