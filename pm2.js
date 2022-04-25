3922528d5be8c347838f867a091e64cfa450e57c
xhayper <hayper1919@gmail.com>
2022-04-25 10:18:59 +0700

    feat: Please, pm2

--- pm2.js
+++ pm2.js
@@ -1,20 +1,21 @@
 require('dotenv/config');
 
 const pm2 = require('pm2'),
-  { exec } = require('child_process'),
   path = require('path');
 
 // exec(`npx pm2 plus`);
 // exec(`npx pm2 link ${process.env.PM2_PUBLIC_KEY} ${process.env.PM2_SECRET_KEY}`);
 
-pm2.connect(true, (err) => {
+pm2.connect((err) => {
   if (err) {
     console.error(err);
     process.exit(2);
   }
 
   pm2.start({
+    autorestart: true,
     script: path.join(__dirname, 'dist/index.js'),
-    name: 'dr-k-bot'
+    name: 'dr-k-bot',
+    source_map_support: true
   });
 });
