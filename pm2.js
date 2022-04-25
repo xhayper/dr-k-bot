1e4777868729616d172d5dbd3d7bfd61d10e8537
xhayper <46065349+xhayper@users.noreply.github.com>
2022-04-25 09:43:04 +0700

    feat: Wooo pm2

--- pm2.js
+++ pm2.js
@@ -1,35 +1,6 @@
 require('dotenv/config');
 
-const pm2 = require('pm2'),
-  { exec } = require('child_process');
+const  { exec } = require('child_process');
 
 exec(`pm2 link ${process.env.PM2_PUBLIC_KEY} ${process.env.PM2_SECRET_KEY}`);
-
-pm2.connect((err) => {
-  if (err) {
-    console.error(err);
-    process.exit(2);
-  }
-
-  pm2.start(
-    {
-      script: 'dist/index.js',
-      name: 'dr-k'
-    },
-    (err) => {
-      if (err) {
-        console.error(err);
-        return pm2.disconnect();
-      }
-
-      pm2.list((err, list) => {
-        console.log(err, list);
-
-        pm2.restart('dr-k', (err, proc) => {
-          // Disconnects from PM2
-          pm2.disconnect();
-        });
-      });
-    }
-  );
-});
+exec(`pm2 start ecosystem.config.js`);
