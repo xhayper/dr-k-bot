dbad502f12cffb6088c40952413f1221eb5eb9b1
xhayper <46065349+xhayper@users.noreply.github.com>
2022-04-25 10:20:07 +0700

    feat: PM2

--- pm2.js
+++ pm2.js
@@ -3,8 +3,8 @@ require('dotenv/config');
 const pm2 = require('pm2'),
   path = require('path');
 
-// exec(`npx pm2 plus`);
-// exec(`npx pm2 link ${process.env.PM2_PUBLIC_KEY} ${process.env.PM2_SECRET_KEY}`);
+exec(`npx pm2 plus`);
+exec(`npx pm2 link ${process.env.PM2_PUBLIC_KEY} ${process.env.PM2_SECRET_KEY}`);
 
 pm2.connect((err) => {
   if (err) {
