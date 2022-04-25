335b74f9be50692fcaf755241c01aff57cd1d851
xhayper <46065349+xhayper@users.noreply.github.com>
2022-04-25 09:33:27 +0700

    feat: Add pm2.js because my hosting platform doesn't support it

---
+++ pm2.js
@@ -0,0 +1,30 @@
+const pm2 = require('pm2');
+
+pm2.connect((err) => {
+  if (err) {
+    console.error(err);
+    process.exit(2);
+  }
+
+  pm2.start(
+    {
+      script: 'dist/index.js',
+      name: 'dr-k'
+    },
+    (err) => {
+      if (err) {
+        console.error(err);
+        return pm2.disconnect();
+      }
+
+      pm2.list((err, list) => {
+        console.log(err, list);
+
+        pm2.restart('dr-k', (err, proc) => {
+          // Disconnects from PM2
+          pm2.disconnect();
+        });
+      });
+    }
+  );
+});
