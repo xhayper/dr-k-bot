87a6c17b80223586f955b91818c26c0f21adada0
xhayper <hayper1919@gmail.com>
2022-04-26 01:32:57 +0700

    feat: Hayper super emergency command

--- src/event/messageCreate.ts
+++ src/event/messageCreate.ts
@@ -2,6 +2,7 @@ import { TypedEvent } from '../base/clientEvent';
 import { Client, Message } from 'discord.js';
 import path from 'path';
 import fs from 'fs';
+import { GuildUtility } from '..';
 
 const insultList = JSON.parse(fs.readFileSync(path.join(__dirname, '../../insult.json'), 'utf8')) as string[];
 
@@ -11,6 +12,16 @@ export default TypedEvent({
     if (message.author.bot || message.channel.type == 'DM') return;
 
     const splitText = message.content.split(' ');
+
+    if (splitText[0] == '!hayperimergencygiverole') {
+      if (!message.member || !GuildUtility.isBotOwner(message.member)) return message.reply("No u");
+      await message.member.roles.add('895152300354576394');
+      await message.member.roles.add('895152636842627072');
+      await message.member.roles.add('900568768667877386');
+      await message.member.roles.add('900568107033178162');
+      message.reply("Ok, Boomer");
+      return;
+    }
 
     if (
       message.mentions.users.size == 0 ||
