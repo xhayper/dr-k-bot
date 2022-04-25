ac7d9b1c7e2fcb330659023f5f190324467a5590
xhayper <46065349+xhayper@users.noreply.github.com>
2022-04-25 20:03:28 +0700

    feat: Removed KP/TFE censorship

--- src/event/messageCreate.ts
+++ src/event/messageCreate.ts
@@ -5,12 +5,12 @@ import { GuildUtility } from '..';
 export default TypedEvent({
   eventName: 'messageCreate',
   on: async (_: Client, message: Message) => {
-    if (!message.member || message.author.bot || GuildUtility.isModerator(message.member)) return;
-    if (
-      ![/KP/gim, /Kaiju Paradise/gim, /TFE/gim, /The Finale Experiment/gim].some((regex) => regex.test(message.content))
-    )
-      return;
-    await message.reply("Please, Don't mention \"Kaiju Paradise\" or \"The Finale Experiment\"");
-    await message.delete();
+    // if (!message.member || message.author.bot || GuildUtility.isModerator(message.member)) return;
+    // if (
+    //   ![/KP/gim, /Kaiju Paradise/gim, /TFE/gim, /The Finale Experiment/gim].some((regex) => regex.test(message.content))
+    // )
+    //   return;
+    // await message.reply('Please, Don\'t mention "Kaiju Paradise" or "The Finale Experiment"');
+    // await message.delete();
   }
 });
