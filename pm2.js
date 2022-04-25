d11211fc8955cfabed4ed08ca33b4abf885006c7
xhayper <hayper1919@gmail.com>
2022-04-25 10:16:06 +0700

    feat: Please

--- pm2.js
+++ pm2.js
@@ -4,8 +4,8 @@ const pm2 = require('pm2'),
   { exec } = require('child_process'),
   path = require('path');
 
-exec(`npx pm2 plus`);
-exec(`npx pm2 link ${process.env.PM2_PUBLIC_KEY} ${process.env.PM2_SECRET_KEY}`);
+// exec(`npx pm2 plus`);
+// exec(`npx pm2 link ${process.env.PM2_PUBLIC_KEY} ${process.env.PM2_SECRET_KEY}`);
 
 pm2.connect(true, (err) => {
   if (err) {
