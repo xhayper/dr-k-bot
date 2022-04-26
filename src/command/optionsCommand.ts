49429ae5a5fd328d73d81c1ae3984f890eb7ff0b
TrixyMixy <74317211+TrixyMixy@users.noreply.github.com>
2022-04-26 15:26:19 -0400

    Create optionsCommand.ts
    Allows for selection of options to adjust, just a menu right now, waoting to actually debug to make sure it works.

---
+++ src/command/optionsCommand.ts
@@ -0,0 +1,32 @@
+import { CommandInteraction, GuildMember, MessageEmbed } from 'discord.js';
+import { SlashCommand } from '../base/slashCommand';
+import { EmbedUtility, GuildUtility } from '..';
+import config from '../config';
+
+export default {
+  data: new SlashCommandBuilder()
+      .setName("options")
+      .setDescription('Adjust Dr. K bot options.')
+      .addSubcommand((sub) =>
+        sub
+          .setName('removeFilter')
+          .setDescription('Removes one of the chat filters.')
+          .addChoices(...config.misc.chatFilters)
+      )
+      .addSubcommand((sub) =>
+        sub
+          .setName('addFilter')
+          .setDescription('Adds a chat filter.')
+          .addStringOption((option) =>
+            option
+            .setName('filter')
+            .setDescription('-')
+            .setRequired(true)
+          )
+      ),
+  guildId: [config.guildId],
+  permission: 'MODERATOR',
+  execute: async (commandInteraction: CommandInteraction) => {
+    // TODO add logic after confirming the menu works.
+  }
+} as SlashCommand;
