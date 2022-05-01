365116e6aa6c6e2cf2d2cc342134143c6012f713
xhayper <hayper1919@gmail.com>
2022-05-01 23:41:39 +0700

    feat: Seems like there's a bug with 'slash-register'

--- src/manager/commandManager.ts
+++ src/manager/commandManager.ts
@@ -1,26 +1,22 @@
-import { APIApplicationCommandBase, SlashRegister } from 'slash-register';
+import { APIApplicationCommand, Routes } from 'discord-api-types/v9';
 import { SlashCommand } from '../base/slashCommand';
 import { Client, Collection } from 'discord.js';
+import { REST } from '@discordjs/rest';
 import glob from 'glob';
 import path from 'path';
 
 export class CommandManager {
+  #client: Client;
+  #rest: REST;
+
   commands: Collection<string, SlashCommand> = new Collection();
 
-  #slashRegister: SlashRegister;
-  #client: Client;
-
   constructor(client: Client) {
-    this.#slashRegister = new SlashRegister();
     this.#client = client;
+    this.#rest = new REST({ version: '9' }).setToken(client.token!);
   }
 
   async reloadCommands() {
-    this.#slashRegister.login(this.#client.token!);
-
-    this.#slashRegister.clearGlobalCommand();
-    this.#slashRegister.clearGuildCommand();
-
     this.commands.clear();
 
     const commandPathList = glob.sync(path.join(__dirname, '../command/*.+(js|ts)'));
@@ -31,16 +27,18 @@ export class CommandManager {
       const commandModule = (await import(command)).default as SlashCommand;
       if (!commandModule.data) continue;
       this.commands.set(commandModule.data.name, commandModule);
-      // if (commandModule.guildId) {
-      //   const commandJSON = commandModule.data.toJSON() as APIApplicationCommandBase;
-      //   for (const guildId of commandModule.guildId) {
-      //     this.#slashRegister.addGuildCommand(guildId, commandJSON);
-      //   }
-      // } else {
-      this.#slashRegister.addGlobalCommand(commandModule.data.toJSON() as APIApplicationCommandBase);
-      // }
     }
 
-    await this.#slashRegister.syncAll();
+    const currentCommand = (await this.#rest.get(
+      Routes.applicationCommands(this.#client.user!.id)
+    )) as APIApplicationCommand[];
+
+    for (const command of currentCommand) {
+      await this.#rest.delete(Routes.applicationCommand(this.#client.user!.id, command.id));
+    }
+
+    this.#rest.put(Routes.applicationCommands(this.#client.user!.id), {
+      body: this.commands.map((slashCommand) => slashCommand.data.toJSON())
+    });
   }
 }
