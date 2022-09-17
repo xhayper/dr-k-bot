28fd93a446354943216362b985fbc5ae514d83f0
xhayper <hayper1919@gmail.com>
2022-09-17 11:21:24 +0700

    feat: bug fix

--- src/manager/commandManager.ts
+++ src/manager/commandManager.ts
@@ -1,96 +1,96 @@
-import { SlashCommand } from '../base/slashCommand';
-import { Client, Collection, RESTPostAPIChatInputApplicationCommandsJSONBody } from 'discord.js';
+import {SlashCommand} from '../base/slashCommand';
+import {Client, Collection, RESTPostAPIChatInputApplicationCommandsJSONBody} from 'discord.js';
 import glob from 'glob';
 import path from 'path';
-import { EqualUtility } from '../utility/equalUtility';
-import { Logger } from '../logger';
+import {EqualUtility} from '../utility/equalUtility';
+import {Logger} from '../logger';
 
 export class CommandManager {
-  private client: Client;
+    private client: Client;
 
-  commands: Collection<string, SlashCommand> = new Collection();
+    commands: Collection<string, SlashCommand> = new Collection();
 
-  constructor(client: Client) {
-    this.client = client;
-  }
-
-  async reloadCommands() {
-    this.commands.clear();
-
-    const commandPathList = glob.sync(path.join(__dirname, '../command/*.+(js|ts)'));
-
-    for (const command of commandPathList) {
-      delete require.cache[require.resolve(command)];
-
-      const commandModule = (await import(command)).default as SlashCommand;
-      if (!commandModule.data) continue;
-      this.commands.set(commandModule.data.name, commandModule);
+    constructor(client: Client) {
+        this.client = client;
     }
 
-    if (!this.client.isReady()) return;
+    async reloadCommands() {
+        this.commands.clear();
 
-    const application = await this.client.application?.fetch();
-    const fetchedGlobalCommands = await application.commands.fetch();
+        const commandPathList = glob.sync(path.join(__dirname, '../command/*.+(js|ts)'));
 
-    for (const commandModule of this.commands.values()) {
-      const globalAPICommand = fetchedGlobalCommands?.find((command) => command.name === commandModule.data.name);
+        for (const command of commandPathList) {
+            delete require.cache[require.resolve(command)];
 
-      if (commandModule.guildId && commandModule.guildId.length > 0) {
-        for (const guildId of commandModule.guildId) {
-          const guild = await this.client.guilds.fetch(guildId);
-          if (!guild) continue;
-
-          Logger.info(`Checking guild command "${commandModule.data.name}" for guild "${guild.name}"`);
-
-          const guildAPICommandList = await guild.commands.fetch();
-          const guildAPICommand = guildAPICommandList.find((command) => command.name === commandModule.data.name);
-
-          if (!guildAPICommand) {
-            Logger.info(`Creating guild command "${commandModule.data.name}" for guild "${guild.name}"`);
-            await guild.commands.create(commandModule.data);
-            // TODO: Remove this when it is in stable
-          } else if (
-            !EqualUtility.isCommandEqual(
-              guildAPICommand,
-              commandModule.data.toJSON() as RESTPostAPIChatInputApplicationCommandsJSONBody
-            )
-          ) {
-            Logger.info(`Updating guild command "${commandModule.data.name}" for guild "${guild.name}"`);
-            await guild.commands.edit(guildAPICommand.id, commandModule.data);
-          }
-
-          guildAPICommandList.forEach((guildCommand) => {
-            const mod = this.commands.get(guildCommand.name);
-            if (mod && mod.guildId && mod.guildId.includes(guildId)) return;
-            Logger.info(`Deleting guild command "${guildCommand.name}" for guild "${guild.name}"`);
-            guildCommand.delete();
-          });
+            const commandModule = (await import(command)).default as SlashCommand;
+            if (!commandModule.data) continue;
+            this.commands.set(commandModule.data.name, commandModule);
         }
 
-        if (globalAPICommand) await globalAPICommand.delete();
-      } else {
-        Logger.info(`Checking global command "${commandModule.data.name}"`);
+        if (!this.client.isReady()) return;
 
-        if (!globalAPICommand) {
-          Logger.info(`Creating global command "${commandModule.data.name}"`);
-          await application.commands.create(commandModule.data);
-          // TODO: Remove this when it is in stable
-        } else if (
-          !EqualUtility.isCommandEqual(
-            globalAPICommand,
-            commandModule.data.toJSON() as RESTPostAPIChatInputApplicationCommandsJSONBody
-          )
-        ) {
-          Logger.info(`Updating global command "${commandModule.data.name}"`);
-          await application.commands.edit(globalAPICommand.id, commandModule.data);
+        const application = await this.client.application?.fetch();
+        const fetchedGlobalCommands = await application.commands.fetch();
+
+        for (const commandModule of this.commands.values()) {
+            const globalAPICommand = fetchedGlobalCommands?.find((command) => command.name === commandModule.data.name);
+
+            if (commandModule.guildId && commandModule.guildId.length > 0) {
+                for (const guildId of commandModule.guildId) {
+                    const guild = await this.client.guilds.fetch(guildId);
+                    if (!guild) continue;
+
+                    Logger.info(`Checking guild command "${commandModule.data.name}" for guild "${guild.name}"`);
+
+                    const guildAPICommandList = await guild.commands.fetch();
+                    const guildAPICommand = guildAPICommandList.find((command) => command.name === commandModule.data.name);
+
+                    if (!guildAPICommand) {
+                        Logger.info(`Creating guild command "${commandModule.data.name}" for guild "${guild.name}"`);
+                        await guild.commands.create(commandModule.data);
+                        // TODO: Remove this when it is in stable
+                    } else if (
+                        !EqualUtility.isCommandEqual(
+                            guildAPICommand,
+                            commandModule.data.toJSON() as RESTPostAPIChatInputApplicationCommandsJSONBody
+                        )
+                    ) {
+                        Logger.info(`Updating guild command "${commandModule.data.name}" for guild "${guild.name}"`);
+                        await guild.commands.edit(guildAPICommand.id, commandModule.data);
+                    }
+
+                    for (const guildCommand of guildAPICommandList.values()) {
+                        const mod = this.commands.get(guildCommand.name);
+                        if (mod && mod.guildId && mod.guildId.includes(guildId)) return;
+                        Logger.info(`Deleting guild command "${guildCommand.name}" for guild "${guild.name}"`);
+                        await guildCommand.delete();
+                    }
+                }
+
+                if (globalAPICommand) await globalAPICommand.delete();
+            } else {
+                Logger.info(`Checking global command "${commandModule.data.name}"`);
+
+                if (!globalAPICommand) {
+                    Logger.info(`Creating global command "${commandModule.data.name}"`);
+                    await application.commands.create(commandModule.data);
+                    // TODO: Remove this when it is in stable
+                } else if (
+                    !EqualUtility.isCommandEqual(
+                        globalAPICommand,
+                        commandModule.data.toJSON() as RESTPostAPIChatInputApplicationCommandsJSONBody
+                    )
+                ) {
+                    Logger.info(`Updating global command "${commandModule.data.name}"`);
+                    await application.commands.edit(globalAPICommand.id, commandModule.data);
+                }
+            }
         }
-      }
+
+        for (const globalCommand of fetchedGlobalCommands.values()) {
+            if (this.commands.get(globalCommand.name)) return;
+            Logger.info(`Deleting global command "${globalCommand.name}"`);
+            await globalCommand.delete();
+        }
     }
-
-    fetchedGlobalCommands?.forEach((globalCommand) => {
-      if (this.commands.get(globalCommand.name)) return;
-      Logger.info(`Deleting global command "${globalCommand.name}"`);
-      globalCommand.delete();
-    });
-  }
 }
