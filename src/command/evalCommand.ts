27c97912eba14891c30ac07c48eaae6539273402
xhayper <hayper1919@gmail.com>
2022-09-19 19:06:24 +0700

    feat: owo

--- src/command/evalCommand.ts
+++ src/command/evalCommand.ts
@@ -1,22 +1,37 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { SlashCommand } from '../base/slashCommand';
+import { inspect } from 'node:util';
 import config from '../config';
 
-export default {
-  data: new SlashCommandBuilder()
-    .setName('eval')
-    .setDescription('-')
-    .addStringOption((input) => input.setName('code').setDescription('-').setRequired(true)),
-  guildId: [config.guildId],
-  permission: 'BOT_OWNER',
-  execute: async (chatInputCommandInteraction: ChatInputCommandInteraction) => {
-    const code = chatInputCommandInteraction.options.getString('code', true);
+const clean = async (text: any) => {
+    if (text && text.constructor.name == "Promise")
+        text = await text;
 
-    try {
-      const result = await eval(code);
-      chatInputCommandInteraction.editReply(result ?? 'No output');
-    } catch (e: any) {
-      chatInputCommandInteraction.editReply(e.toString());
+    if (typeof text !== "string")
+        text = inspect(text, { depth: 1 });
+
+    text = text
+        .replace(/`/g, "`" + String.fromCharCode(8203))
+        .replace(/@/g, "@" + String.fromCharCode(8203));
+
+    return text;
+}
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName('eval')
+        .setDescription('-')
+        .addStringOption((input) => input.setName('code').setDescription('-').setRequired(true)),
+    guildId: [config.guildId],
+    permission: 'BOT_OWNER',
+    execute: async (chatInputCommandInteraction: ChatInputCommandInteraction) => {
+        const code = chatInputCommandInteraction.options.getString('code', true);
+
+        try {
+            const result = await clean(eval(code));
+            chatInputCommandInteraction.editReply(result ?? 'No output');
+        } catch (e: any) {
+            chatInputCommandInteraction.editReply(e.toString());
+        }
     }
-  }
 } as SlashCommand;
