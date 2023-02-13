0f0ff5d5468337cb7e96fdfc12ffdcc851baea10
xhayper <46065349+xhayper@users.noreply.github.com>
2023-02-13 10:09:33 +0700

    feat: also make it so the button warn the user about it

--- src/interaction-handlers/verificationModalButton.ts
+++ src/interaction-handlers/verificationModalButton.ts
@@ -2,12 +2,17 @@ import { InteractionHandler, InteractionHandlerTypes } from '@sapphire/framework
 import { ModalUtility } from '../utility/modalUtility';
 import { ApplyOptions } from '@sapphire/decorators';
 import { type ButtonInteraction } from 'discord.js';
+import { VerificationUtility } from '..';
 
 @ApplyOptions<InteractionHandler.Options>({
   interactionHandlerType: InteractionHandlerTypes.Button
 })
 export class Handler extends InteractionHandler {
   public async run(interaction: ButtonInteraction) {
+    if (await VerificationUtility.getTicketsFromUserId(interaction.user.id)) {
+      return void (await interaction.editReply({ content: 'You already have a ticket! Please be patient!' }));
+    }
+
     await interaction.showModal(ModalUtility.createApplicationModal());
   }
 
