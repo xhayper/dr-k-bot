1f81383eeebce17d5bba31f96231ca7c679baf7c
xhayper <hayper1919@gmail.com>
2024-01-16 13:31:36 +0700

    feat: apply primsa recommendation on having 1 global instance

---
+++ src/database.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client';
+import { container } from '@sapphire/pieces';
+
+const database = new PrismaClient({
+  log: [
+    { emit: 'event', level: 'query' },
+    { emit: 'event', level: 'error' },
+    { emit: 'event', level: 'info' },
+    { emit: 'event', level: 'warn' }
+  ]
+});
+
+database.$on('query', (query) => {
+  container.logger.debug('Database:', query.query, `(${query.params.slice(1, -1)})`, `(took ${query.duration}ms)`);
+});
+
+database.$on('error', (err) => {
+  container.logger.info('Database:', err.message);
+});
+
+database.$on('info', (info) => {
+  container.logger.info('Database:', info.message);
+});
+
+database.$on('warn', (warn) => {
+  container.logger.warn('Database:', warn.message);
+});
+
+container.database = database;
+
+// Declare extra properties on the container
+declare module '@sapphire/pieces' {
+  interface Container {
+    database: typeof database;
+  }
+}
+
+export { database };
