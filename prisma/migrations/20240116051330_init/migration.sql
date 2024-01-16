e4b61ea16888c2571d641da47a799a8e2b2269a2
xhayper <hayper1919@gmail.com>
2024-01-16 12:13:41 +0700

    feat: add logMessageId to the database schema

--- prisma/migrations/20240116051330_init/migration.sql
+++ prisma/migrations/20240116051330_init/migration.sql
@@ -1,20 +1,24 @@
 -- CreateTable
 CREATE TABLE "VerificationRequest" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "ownerId" TEXT NOT NULL
+    "ownerId" TEXT NOT NULL,
+    "logMessageId" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "VerificationAnswer" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "question" TEXT NOT NULL,
-    "answer" BLOB NOT NULL,
+    "answer" TEXT NOT NULL,
     "verificationRequestId" TEXT,
     CONSTRAINT "VerificationAnswer_verificationRequestId_fkey" FOREIGN KEY ("verificationRequestId") REFERENCES "VerificationRequest" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "VerificationRequest_ownerId_key" ON "VerificationRequest"("ownerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationRequest_logMessageId_key" ON "VerificationRequest"("logMessageId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "VerificationAnswer_question_key" ON "VerificationAnswer"("question");
