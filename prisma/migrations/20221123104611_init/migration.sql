ded12d2878e7c6012606a4fcbdd3f1c8ce80e82e
xhayper <46065349+xhayper@users.noreply.github.com>
2025-10-09 23:31:26 +0700

    feat: DO NOT ignore migration

---
+++ prisma/migrations/20221123104611_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "VerificationTicket" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "discordId" TEXT NOT NULL,
+    "messageId" TEXT,
+    "answers" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationTicket_messageId_key" ON "VerificationTicket"("messageId");
